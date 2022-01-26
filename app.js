a4dee52c2546035df43b8e536fa9ed8c5d423e81
mabhyHs <m4huens@gmail.com>
2022-01-26 19:23:22 -0300

    optimized code

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
-
-    document.querySelector('#red').onclick = function(){
-        document.querySelector('#hello').style.color = 'red';
-
-    }
-    document.querySelector('#blue').onclick = function(){
-        document.querySelector('#hello').style.color = 'blue';
-
-    }
-    document.querySelector('#green').onclick = function(){
-        document.querySelector('#hello').style.color = 'green';
-
-    }
+   document.querySelectorAll('button').forEach(function(button){
+         button.onclick = function(){
+            document.querySelector('#hello').style.color = button.dataset.color;
+         }
+   });
 
 });
