eabdfb64ba5573f07459f14116c616ad94519f3b
mabhyHs <m4huens@gmail.com>
2022-01-26 19:09:52 -0300

    v1.0

---
+++ app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    document.querySelector('#red').onclick = function(){
+        document.querySelector('#hello').style.color = 'red';
+
+    }
+    document.querySelector('#blue').onclick = function(){
+        document.querySelector('#hello').style.color = 'blue';
+
+    }
+    document.querySelector('#green').onclick = function(){
+        document.querySelector('#hello').style.color = 'green';
+
+    }
+
+});
