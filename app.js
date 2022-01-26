f7dc8eadb19a9cbdda07cbdc8211f5fc1ccc421b
mabhyHs <m4huens@gmail.com>
2022-01-26 19:39:04 -0300

    v 2.0 - adding select instead of buttons

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-   document.querySelectorAll('button').forEach( button => {
-         button.onclick = () => {
-            document.querySelector('#hello').style.color = button.dataset.color;
-         }
-   });
+
+        document.querySelector('select').onchange = function() {
+            document.querySelector('#hello').style.color = this.value;
+        }
 
 });
